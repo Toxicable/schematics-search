dd8d1d9c4ccdb29883815eb7cadd0f0dd4874e41
Fabian Wiles <fabianwiles@live.com>
2018-03-25 05:08:50 +1200

    add firebase deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { MatInputModule} from '@angular/material';
-import { MatProgressBarModule} from '@angular/material';
+import { MatInputModule} from '@angular/material/input';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
